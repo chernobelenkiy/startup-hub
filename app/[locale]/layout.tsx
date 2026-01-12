f5f881b00177a50bd06515a0a83a945da0d77cf9
Ivan Bunin <chernobelenkiy@gmail.com>
2026-01-12 14:55:21 +0300

    fix favicon

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -34,6 +34,10 @@ export const metadata: Metadata = {
     "investment",
     "MVP",
   ],
+  icons: {
+    icon: "/favicon.svg",
+    apple: "/favicon.svg",
+  },
 };
 
 export function generateStaticParams() {
